aac9a0f8cd409771bb1c09d8f395fc3016476c90
Maike <maikonalexandre574@gmail.com>
2024-04-13 07:29:25 -0300

    feat: create simple home page

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import './style.css'
 import "../src/components/min-preview/index.js"
 
 document.querySelector('#app').innerHTML = /*html*/`
-  <div>
-    <min-preview/>
-  </div>
+<div class="t">
+    <div>
+        <h1>Theme <span class="title-secondary">Hub</span></h1>
+    </div>
+
+    <div class="main-grid">
+        <min-preview primary="#32A6DC" title="Theme pink" secondary="#A370FB" danger="#a25544" success="#bbeeee" warning="#12f2ff" id="1"></min-preview>
+    </div>
+</div>
 `
+
+
 
