22f592bc9b10133723aa3c3c282a456fb8d34878
Maike <maikonalexandre574@gmail.com>
2024-04-14 18:46:07 -0300

    feat: create storage and storage with localStorage

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,12 +1,111 @@
-class Store {
+import themeMock from "./themes.json"
+import cuuid from "cuuid"
+
+class Storage {
     constructor() {
-        this.value = 50
+        this.storageThemes = themeMock
     }
 
-    setValue(number) {
-        this.value = number
+    getThemes() {
+        return this.storageThemes
     }
+
+    getThemeById(id) {
+        return this.storageThemes.find((theme) => theme.id == id)
+    }
+
+    deleteTheme(id) {
+        const themesNotBeDeleted = this.storageThemes.filter((e) => e.id != id)
+        this.storageThemes = themesNotBeDeleted
+    }
+
+    saveThemeOnStorage(theme, id) {
+        if (id) {
+            const themesThatNotBeEdited = this.storageThemes.filter((e) => e.id != id)
+            const editedTheme = { ...theme, id }
+            this.storageThemes = [...themesThatNotBeEdited, editedTheme]
+            return
+        }
+
+        const newTheme = [...this.storageThemes, { ...theme, id: cuuid() }]
+        this.storageThemes = newTheme
+    }
+
 }
 
 
-export const store = new Store()
+
+class ThemesStorageWithLocalStorage {
+    constructor() {
+        this.updateState()
+    }
+
+    saveThemeOnStorage(data, id) {
+        if (id) {
+            this.updateThemeOnStorage(data, id)
+        }
+        if (!id) {
+            this.createThemeOnStorage(data)
+        }
+    }
+
+    getThemesOnStorage() {
+        return JSON.parse(localStorage.getItem("@theme-hub-themes"))
+    }
+
+    createThemeOnStorage(data) {
+        const currentThemes = this.getThemesOnStorage()
+        const newTheme = [{ ...data, id: cuuid() }]
+
+        if (currentThemes) {
+            const allThemes = [...currentThemes, ...newTheme]
+            localStorage.setItem("@theme-hub-themes", JSON.stringify(allThemes));
+            this.storageThemes = allThemes
+            this.updateState()
+            return
+        }
+
+        localStorage.setItem("@theme-hub-themes", JSON.stringify(newTheme));
+        this.updateState()
+    }
+
+    updateThemeOnStorage(data, id) {
+        const currentThemes = this.getThemesOnStorage()
+        const themesThatNotBeEdited = currentThemes.filter((theme) => theme.id != id)
+        const updatedTheme = { ...data, id }
+
+
+        const allThemes = [...themesThatNotBeEdited, updatedTheme]
+        localStorage.setItem("@theme-hub-themes", JSON.stringify(allThemes));
+        this.updateState()
+    }
+
+    getThemes() {
+        return this.storageThemes
+    }
+
+    getThemeById(id) {
+        return this.storageThemes.find((theme) => theme.id == id)
+    }
+
+    deleteTheme(id) {
+        const themesNotBeDeleted = this.getThemesOnStorage().filter((theme) => theme.id != id)
+        localStorage.setItem("@theme-hub-themes", JSON.stringify(themesNotBeDeleted));
+        this.updateState()
+    }
+
+    updateState() {
+        const currentThemesOnStorage = this.getThemesOnStorage()
+        if (currentThemesOnStorage) {
+            this.storageThemes = [...themeMock, ...currentThemesOnStorage]
+            return
+        }
+
+        this.storageThemes = [...themeMock]
+    }
+
+}
+
+
+export const themesStorage = new ThemesStorageWithLocalStorage()
+// export const themesStorage = new Storage()
